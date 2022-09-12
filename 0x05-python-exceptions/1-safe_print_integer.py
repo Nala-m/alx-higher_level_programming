f86580cb86c7b840d7ce55ec6eee41b7c27946cb
Nala-m <marylindao@yahoo.com>
2022-09-12 11:04:58 -0700

    hot fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -9,8 +9,7 @@ def safe_print_integer(value):
         Otherwise - True.
     """
     try:
-        print("{d}".format(value))
+        print("{:d}".format(value))
+        return (True)
     except (TypeError, ValueError):
         return (False)
-    else:
-        return (True)
