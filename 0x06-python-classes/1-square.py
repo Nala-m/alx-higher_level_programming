4eea481f17f2441bcff2f58220c90b66c09b803b
Nala-m <marylindao@yahoo.com>
2022-09-14 06:23:18 -0700

    i square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-python3 - c 'print(__import__("my_module").__doc__)'
+
+"""Define a class Square."""
 
 
 class Square:
-    python3 - c 'print(__import__("my_module").MyClass.__doc__)'
-    python3 - c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-    """Class Square that defines a square object
-    """
+    """Represent a square."""
+
     def __init__(self, size):
-        """Initialize method that stores the size of the square
+        """Initialize a new Square.
         Args:
-            param1 (int): size of the square
+            size (int): The size of the new square.
         """
         self.__size = size
