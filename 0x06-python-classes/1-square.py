ae46256581266df8ff14c74d29632418c7b10dfb
Nala-m <marylindao@yahoo.com>
2022-09-14 06:10:59 -0700

    1 square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+python3 - c 'print(__import__("my_module").__doc__)'
+
+
 class Square:
+    python3 - c 'print(__import__("my_module").MyClass.__doc__)'
+    python3 - c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """Class Square that defines a square object
     """
     def __init__(self, size):
