26240a3c46d3b2ccf8a2c5688e31e364e1a51f6a
Nala-m <marylindao@yahoo.com>
2022-09-21 09:47:59 -0700

    0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-¨¨¨Defines an integer addition function¨¨¨
+
+"""Module for 0-add_integer.It contains the function add_integer
+which adds two integers and returns their sum
+"""
 
 
 def add_integer(a, b=98):
-    ¨¨¨Returns the sum of two integers
+    """returns the sum of two integers
     Args:
         a = the first integer
         b = the second integer
     Return:
         The sum of a and b
-    ¨¨¨
+    """
     if type(a) is float:
         a = int(a)
     if type(b) is float:
