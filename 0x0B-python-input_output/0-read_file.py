95e7365cbf709a2b7a637b22536b89648e24d79d
Nala-m <marylindao@yahoo.com>
2022-09-27 11:17:59 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Module that holds a function that reads a file"""
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    """Function that reads from a file
-
+    """ Function that reads from a file
     Args:
         filename: filename
-    Raises:
+    Raises
         Exception: when the file can be opened
     """
+
     with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
         print(read_data, end='')
-
-
-
