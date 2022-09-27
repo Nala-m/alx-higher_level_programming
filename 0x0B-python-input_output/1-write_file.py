7dc556267853aed30f2e95e1d26770c1b119ba3d
Nala-m <marylindao@yahoo.com>
2022-09-27 09:36:59 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,12 +4,11 @@
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
-
     Args:
-        filename: The name of the file.
+        filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
-        The number of characters written
+        The number of characters written.
     """
-    with open(filename, "w", encoding-"utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
