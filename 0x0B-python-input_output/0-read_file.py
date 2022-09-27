ceb0f6dc678b750d2db3d8d3c89aa1b642d6a50e
Nala-m <marylindao@yahoo.com>
2022-09-27 11:02:26 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+"""Module that holds a function that reads a file"""
+
 
 def read_file(filename=""):
-    """Print the contents of UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: the file to read
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
+
+
